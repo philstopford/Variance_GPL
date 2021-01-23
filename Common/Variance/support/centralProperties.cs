dcd8b0aaf6f3a97bfc9ead44ee9ce6683df3da12
philstopford <phil.stopford@gmail.com>
2021-01-23 17:08:48 -0600

    b1

--- Common/Variance/support/centralProperties.cs
+++ Common/Variance/support/centralProperties.cs
@@ -3,7 +3,7 @@
     public static class CentralProperties
     {
         public const string productName = "Variance";
-        public const string version = "4.4a1";
+        public const string version = "4.4b1";
         public const int maxLayersForMC = 16; // maximum number of supported layers in MC system
         public const int scaleFactorForOperation = 10000;
         public enum typeShapes { none, rectangle, L, T, X, U, S, GEOCORE, BOOLEAN };
