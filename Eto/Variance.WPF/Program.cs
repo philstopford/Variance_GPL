c50a5bab3ebd5df6d70a8187c2b89f9fbe5282af
philstopford <phil.stopford@gmail.com>
2021-04-22 07:39:48 -0500

    Update Program.cs

--- Eto/Variance.WPF/Program.cs
+++ Eto/Variance.WPF/Program.cs
@@ -58,7 +58,7 @@ namespace Variance.WPF
                     try
                     {
                         string[] tokens = args[i].Split(new char[] { '.' });
-                        string extension = tokens[tokens.Length - 1];
+                        string extension = tokens[^1];
                         if ((extension.ToUpper() == "VARIANCE") || (extension.ToUpper() == "XML"))
                         {
                             xmlFile = args[i];
