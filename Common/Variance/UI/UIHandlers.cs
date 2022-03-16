535d34b319caf9b8f2ddc4a83b8967918c8b826e
philstopford <phil.stopford@gmail.com>
2022-03-16 14:12:47 -0500

    Update UIHandlers.cs

--- Common/Variance/UI/UIHandlers.cs
+++ Common/Variance/UI/UIHandlers.cs
@@ -11,7 +11,7 @@ public partial class MainForm
         if (aboutBox is not {Visible: true})
         {
             string creditText = "Version " + commonVars.version + ", " +
-                                "© " + commonVars.author + " 2014-2021" + "\r\n\r\n";
+                                "© " + commonVars.author + " 2014-2022" + "\r\n\r\n";
             creditText += varianceContext.vc.licenceName;
             creditText += "\r\n\r\n";
             creditText += "Libraries used:\r\n";
